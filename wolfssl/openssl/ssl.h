89ed97fe976882ba165135bd102d1f88071bdac2
Carie Pointer <carie@wolfssl.com>
2018-09-25 14:10:09 -0700

    Add additional defines for QT compatibility

--- wolfssl/openssl/ssl.h
+++ wolfssl/openssl/ssl.h
@@ -266,6 +266,11 @@ typedef WOLFSSL_X509_STORE_CTX X509_STORE_CTX;
 #define SSL_SESSION_get_master_key      wolfSSL_SESSION_get_master_key
 #define SSL_SESSION_get_master_key_length wolfSSL_SESSION_get_master_key_length
 
+#ifdef WOLFSSL_QT
+#define SSL_MODE_RELEASE_BUFFERS    wolfSSL_SSL_MODE_RELEASE_BUFFERS
+#define SSL_CTRL_SET_TMP_ECDH       wolfSSL_SSL_CTRL_SET_TMP_ECDH
+#endif
+        
 #define DSA_dup_DH                      wolfSSL_DSA_dup_DH
  
 #define i2d_X509_bio                    wolfSSL_i2d_X509_bio
