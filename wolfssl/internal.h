78966e146a4a7a0f968cdbe9d3b642e55234bf11
Takashi Kojo <kojo@wolfsssl.com>
2019-02-22 05:39:38 +0900

    add wolfSSL_CertManagerCheckOCSP_Staple

--- wolfssl/internal.h
+++ wolfssl/internal.h
@@ -1803,6 +1803,7 @@ struct WOLFSSL_CIPHER {
 #endif
 
 /* wolfSSL OCSP controller */
+#ifdef HAVE_OCSP
 struct WOLFSSL_OCSP {
     WOLFSSL_CERT_MANAGER* cm;            /* pointer back to cert manager */
     OcspEntry*            ocspList;      /* OCSP response list */
@@ -1812,6 +1813,7 @@ struct WOLFSSL_OCSP {
     int(*statusCb)(WOLFSSL*, void*);
 #endif
 };
+#endif 
 
 #ifndef MAX_DATE_SIZE
 #define MAX_DATE_SIZE 32
